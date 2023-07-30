3450226d490ddb100faae8b4807a44bf835c5501
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2023-07-30 09:00:21 -0300

    primeiro comit

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+# Create your views here.
+def cadastro(request):
+    return render(request, 'core/form.html')
+
+def lista(request):
+    return render(request, 'core/lista.html')
+
+
+def detalhe(request):
+    return render(request, 'core/detalhe.html')
