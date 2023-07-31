da15f7c5cfb784296df5bbbaa8b9732a803c8918
Jeferson Queiroga <jefersonqueiroga@gmail.com>
2023-07-31 05:33:23 -0300

    adicionando url

--- main/urls.py
+++ main/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from core.views import index,cadastro,lista,detalhe
+from core.views import index,cadastro,lista
 
 urlpatterns = [
     path('admin/', admin.site.urls),
